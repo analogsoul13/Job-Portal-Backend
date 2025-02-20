7ca4893b57397e33024a64e7479bcd695e4d73ed
analogsoul13 <analogsoul13@gmail.com>
2025-02-20 22:56:10 +0530

    changed to diskstorage

--- Middlewares/multer.js
+++ Middlewares/multer.js
@@ -1,4 +1,20 @@
-import multer from "multer"
+const multer = require('multer');
 
-const storage = multer.memoryStorage();
-export const singleUpload = multer({storage}).single("file"); // the name file
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads'); // Store files in the "uploads" folder
+    },
+    filename: (req, file, cb) => {
+        cb(null, `Image-${Date.now()}-${file.originalname}`); // Generate unique filenames
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(new Error("The file must be in JPG, JPEG, or PNG format"));
+    }
+};
+
+module.exports = multer({ storage, fileFilter });

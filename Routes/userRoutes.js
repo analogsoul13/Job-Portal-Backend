effbefbedc9d5651344b7343442e056d8b682329
analogsoul13 <analogsoul13@gmail.com>
2025-02-25 14:18:43 +0530

    Multer import fix

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const { register, login, updateProfile, logout, getProfile } = require('../Controllers/userController')
 const isAuthenticated = require('../Middlewares/isAuthenticated')
-const multer = require('multer')
+const multer = require('../Middlewares/multer')
 
 const router = express.Router()
 
 router.route("/register").post(register)
 router.route("/login").post(login)
 router.route("/logout").get(logout)
-router.route("/profile/update").post(multer,isAuthenticated,updateProfile)
+router.route("/profile/update").put(multer,isAuthenticated,updateProfile)
 router.route("/profile").get(isAuthenticated,getProfile)
 
 module.exports = router

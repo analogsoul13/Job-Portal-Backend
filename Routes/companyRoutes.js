6f825e2b15cd518a9fdf4920ac62ba7a98250cf3
analogsoul13 <analogsoul13@gmail.com>
2025-07-18 17:13:32 +0530

    Multer added

--- Routes/companyRoutes.js
+++ Routes/companyRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const isAuthenticated = require('../Middlewares/isAuthenticated')
 const { registerCompany, getCompany, getCompanyById, updateCompany } = require('../Controllers/companyController')
+const multer = require('../Middlewares/multer')
 
 const router = express.Router()
 
-router.route("/register").post(isAuthenticated,registerCompany)
+router.route("/register").post(multer,isAuthenticated,registerCompany)
 router.route("/get").get(isAuthenticated,getCompany)
 router.route("/get/:id").get(isAuthenticated,getCompanyById)
-router.route("/update/:id").put(isAuthenticated,updateCompany)
+router.route("/update/:id").put(multer,isAuthenticated,updateCompany)
 
 module.exports = router

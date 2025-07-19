49a9dd1460aaac0e7bf16976f2525ec1bc865ce3
analogsoul13 <analogsoul13@gmail.com>
2025-07-19 18:37:00 +0530

    Delete Route

--- Routes/companyRoutes.js
+++ Routes/companyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const isAuthenticated = require('../Middlewares/isAuthenticated')
-const { registerCompany, getCompany, getCompanyById, updateCompany } = require('../Controllers/companyController')
+const { registerCompany, getCompany, getCompanyById, updateCompany, deletCompany } = require('../Controllers/companyController')
 const multer = require('../Middlewares/multer')
 
 const router = express.Router()
@@ -9,5 +9,6 @@ router.route("/register").post(multer,isAuthenticated,registerCompany)
 router.route("/get").get(isAuthenticated,getCompany)
 router.route("/get/:id").get(isAuthenticated,getCompanyById)
 router.route("/update/:id").put(multer,isAuthenticated,updateCompany)
+router.route("/delete/:id").delete(isAuthenticated,deletCompany)
 
 module.exports = router
